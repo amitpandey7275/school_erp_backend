8b825f37cb994d567a4d70bc70e64d907886222f
Amit Pandey <ap6238969@gmail.com>
2026-01-14 19:06:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-app.get("/", (req, res) => {
-  res.send("SERVER OK");
-});
 
 require("dotenv").config();
 const express = require("express");
@@ -1496,6 +1493,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
 
 
